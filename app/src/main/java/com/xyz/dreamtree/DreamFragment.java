730d0a365633a17bec49b033939d1cdb5ca87c37
naman14 <namandwivedi14@gmail.com>
2015-03-13 23:10:12 +0530

    sdk 15

--- app/src/main/java/com/xyz/dreamtree/DreamFragment.java
+++ app/src/main/java/com/xyz/dreamtree/DreamFragment.java
@@ -6,17 +6,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by naman on 13/03/15.
  */
 public class DreamFragment extends Fragment {
 
 
+
+    public String loadDreamsFromFile() {
+
+        String jsonString = "";
+        try {
+            String currentLine;
+            File cacheFile = new File(getActivity().getFilesDir(), "dreams.json");
+
+            BufferedReader br = new BufferedReader(new FileReader(cacheFile));
+            while ((currentLine = br.readLine()) != null) {
+                jsonString += currentLine + '\n';
+            }
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        return jsonString;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         final View v = inflater.inflate(R.layout.fragment_dream, container, false);
+
+
+        final File cacheFile = new File(getActivity().getFilesDir(), "dreams.json");
+
+     
+
+
 
         return v;
 
