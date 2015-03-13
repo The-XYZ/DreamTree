8a02cfa2da89b796c9a9ef76dd2793d26fddbb3c
Laavanye Bahl <laavanye.bahl@gmail.com>
2015-03-14 02:14:27 +0530

    ignore
    Signed-off-by: Laavanye Bahl <laavanye.bahl@gmail.com>

---
+++ app/src/main/java/com/xyz/dreamtree/DreamEntity.java
@@ -0,0 +1,14 @@
+package com.xyz.dreamtree;
+
+/**
+ * Created by yogesh on 14/3/15.
+ */
+public class DreamEntity {
+    public int imageResId;
+    public int titleResId;
+
+    public DreamEntity (int imageResId, int titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+}
