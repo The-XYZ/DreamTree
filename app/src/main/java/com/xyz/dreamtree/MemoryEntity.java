8e541f40d9bc987153e3661fd0a8991689bd079a
yogeshbalan <yogeshbalan@live.co.uk>
2015-03-14 02:37:10 +0530

    FAB

---
+++ app/src/main/java/com/xyz/dreamtree/MemoryEntity.java
@@ -0,0 +1,15 @@
+package com.xyz.dreamtree;
+
+/**
+ * Created by yogesh on 14/3/15.
+ */
+public class MemoryEntity {
+    public int imageResId;
+    public int titleResId;
+
+    public MemoryEntity (int imageResId, int titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+
+}
