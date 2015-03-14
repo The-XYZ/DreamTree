ab438ba1dd5b41efd1fd71fd2b98dc46de0ecb52
Laavanye Bahl <laavanye.bahl@gmail.com>
2015-03-14 11:11:17 +0530

    putting music
    Signed-off-by: Laavanye Bahl <laavanye.bahl@gmail.com>

---
+++ app/src/main/java/com/xyz/dreamtree/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.xyz.dreamtree;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+    private final int SPLASH_DISPLAY_LENGHT = 3500;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.happy);
+        mPlayer.start();
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+/* New Handler to start the Menu-Activity
+* and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+/* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+
+
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
